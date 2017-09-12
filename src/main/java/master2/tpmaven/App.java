c0b5e34333203acce0701b9d3584e688776ac1e1
adou <loic.soukpa@gmail.com>
2017-09-12 17:45:39 +0200

    ajout d'un projet

---
+++ src/main/java/master2/tpmaven/App.java
@@ -0,0 +1,19 @@
+package master2.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void sese ()
+    {
+    	int a;
+        System.out.println( "Code Mort!" );
+    }
+}
